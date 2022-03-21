62e0155621c7c9b87856c2a350f7be630017df24
LinGaOscar <oscar987123@yahoo.com.tw>
2022-03-21 13:49:20 +0800

    set

--- RoundTable/src/main/java/com/javaclass/roundtable/controller/LoginController.java
+++ RoundTable/src/main/java/com/javaclass/roundtable/controller/LoginController.java
@@ -1,10 +1,17 @@
 package com.javaclass.roundtable.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("/api")
 public class LoginController {
 
+	@PostMapping("/login")
+	public String userLogin() {
+		return null;
+		
+	}
 }
