c5560c1b84416b940f02af1e793a082f687c6629
LinGaOscar <oscar987123@yahoo.com.tw>
2022-03-21 17:38:27 +0800

    set

---
+++ src/main/java/com/javaclass/roundtable/RoundTableApplication.java
@@ -0,0 +1,13 @@
+package com.javaclass.roundtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoundTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoundTableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaclass/roundtable/RoundTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaclass.roundtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
