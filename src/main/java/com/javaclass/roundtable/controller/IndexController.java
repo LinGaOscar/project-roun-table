637d816fbc09ad4492d85088a91fcaa7438505e0
LinGaOscar <oscar987123@yahoo.com.tw>
2022-03-28 17:29:51 +0800

    set

--- src/main/java/com/javaclass/roundtable/controller/IndexController.java
+++ src/main/java/com/javaclass/roundtable/controller/IndexController.java
@@ -1,23 +1,49 @@
 package com.javaclass.roundtable.controller;
 
+import com.javaclass.roundtable.entity.ClassTable;
 import com.javaclass.roundtable.entity.SysUser;
+import com.javaclass.roundtable.service.ClassTableServiceImpl;
+import com.javaclass.roundtable.service.SysUserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 import java.util.Objects;
 
 @Controller
 public class IndexController {
-    @GetMapping({"/"})
-    public String indexPage() {
-//        return "login";
-        return "test";
+    private SysUserServiceImpl sysUserService;
+    private ClassTableServiceImpl classTableService;
+
+    @Autowired
+    public void autoWired(SysUserServiceImpl sysUserService, ClassTableServiceImpl classTableService) {
+        this.sysUserService = sysUserService;
+        this.classTableService = classTableService;
     }
 
-    @GetMapping({"/timeTable"})
-    public String timeTablePage() {
+
+    @GetMapping({"/"})
+    public String indexPage() {
+        return "login";
+//        return "test";
+    }
+
+    @GetMapping({"/classTable"})
+    public String timeTablePage(Model model) {
+        List<ClassTable> classTables = classTableService.findAll();
+        model.addAttribute("classList", classTables);
+
         return "class_table";
+    }
+
+    @GetMapping({"/userTable"})
+    public String userTablePage(Model model) {
+        List<SysUser> sysUsers = sysUserService.findAll();
+        model.addAttribute("userList", sysUsers);
+
+        return "user_table";
     }
 
     @GetMapping({"/index"})
