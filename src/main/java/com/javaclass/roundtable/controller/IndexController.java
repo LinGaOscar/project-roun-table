bd53ea2bf648799eefc68687776e3bee1131a874
LinGaOscar <oscar987123@yahoo.com.tw>
2022-04-08 15:49:43 +0800

    set login logout

--- src/main/java/com/javaclass/roundtable/controller/IndexController.java
+++ src/main/java/com/javaclass/roundtable/controller/IndexController.java
@@ -9,29 +9,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Objects;
 
 @Controller
 public class IndexController {
 
-    @GetMapping({"/"})
-    public String indexPage() {
-        return "login";
-//        return "test";
-    }
+    @GetMapping({"/", "/index"})
+    public String indexPage(HttpSession httpSession) {
 
-
-
-    @GetMapping({"/index"})
-    public String loginPage(Model model) {
-        SysUser sysUser = (SysUser) model.asMap().get("user");
-//        System.out.println(model.addAttribute("user"));
-//        System.out.println(sysUser);
-        if (Objects.isNull(sysUser)) {
-
+        if (Objects.isNull(httpSession.getAttribute("loginCheck"))) {
+            return "/login";
         }
-//        System.out.println(model.getAttribute("user"));
         return "index";
     }
+
 }
--- src/main/java/com/javaclass/roundtable/controller/LoginController.java
+++ src/main/java/com/javaclass/roundtable/controller/LoginController.java
@@ -5,15 +5,16 @@ import com.javaclass.roundtable.service.SysUserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
 import java.util.Objects;
 
 @Controller
-@RequestMapping("/api")
 public class LoginController {
     private SysUserServiceImpl sysUserService;
 
@@ -23,21 +24,35 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String account, @RequestParam String password, Model model, RedirectAttributes redirectAttrs) {
-        System.out.println(account + password);
+    public String login(@RequestParam String account, @RequestParam String password, Model model,HttpSession httpSession) {
+
+        if (httpSession.getAttribute("loginCheck")=="ok"){
+            return "redirect:/index";
+        }
+
         SysUser sysUser = sysUserService.findByAccount(account);
-        System.out.println(sysUser);
         if (Objects.isNull(sysUser)) {
             model.addAttribute("userError", "not found user");
             return "login";
-        } else {
-            if (sysUser.getPassword().equals(password)) {
-                redirectAttrs.addFlashAttribute("user", sysUser);
-                return "redirect:/index";
-            } else {
-                model.addAttribute("passwordError", "incorrect password");
-                return "login";
-            }
         }
+        if (sysUser.getPassword().equals(password)) {
+            httpSession.setAttribute("loginCheck","ok");
+            httpSession.setAttribute("userName",sysUser.getUserName());
+            httpSession.setAttribute("userAccount",sysUser.getAccount());
+            return "redirect:/index";
+        } else {
+            model.addAttribute("passwordError", "incorrect password");
+            return "login";
+        }
+
+
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession httpSession) {
+        httpSession.removeAttribute("loginCheck");
+        httpSession.removeAttribute("userName");
+        httpSession.removeAttribute("userAccount");
+        return "login";
     }
 }
